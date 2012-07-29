5102679a729960da0ae805ec50b91d425c52f7ae
Winfred <winfred.nadeau@gmail.com>
2012-07-29 12:04:15 -0400

    adding grunt.js for build scripting

---
+++ grunt.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+		concat: {
+			dist: {
+				src: ['lib/*.js', 'lib/**/*.js'],
+				dest: 'build/whiteboard.js'
+			}
+		},
+    lint: {
+      all: ['grunt.js', 'lib/**/*.js','test/**/*.js']
+    },
+    jshint: {
+      options: {
+        browser: true
+      }
+    },
+		min: {
+      dist: {
+        src:  '<config:concat.dist.dest>',
+        dest: 'build/whiteboard.min.js'
+      }
+		},	
+		watch: {
+			files: '<config:concat.dist.src>',
+			tasks: 'concat'
+		}
+  });
+  // Default task.
+  grunt.registerTask('default', 'concat min');
+
+};
