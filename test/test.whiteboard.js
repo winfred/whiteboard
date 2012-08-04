b62ad1fb84cd0293e01e239f0b8507f3bd2a8816
Winfred <winfred.nadeau@gmail.com>
2012-08-03 23:15:19 -0400

    test.whiteboard uses new event target pattern

--- test/test.whiteboard.js
+++ test/test.whiteboard.js
@@ -1,7 +1,7 @@
 describe("whiteboard", function() {
   var _ = whiteboard;
   describe("events", function() {
-    var stroke = {id: "hey"};
+    var target = {id: "hey"};
 
     describe("#on", function() {
       it("binds a function to an event", function() {
@@ -9,7 +9,7 @@ describe("whiteboard", function() {
         _.on("StrokeAction.focus.commit", function(){
           called = true;
         });
-        _.emit('StrokeAction.focus.commit', {stroke: stroke});
+        _.emit('StrokeAction.focus.commit', {target: target});
         expect(called).to.be(true);
       });
 
@@ -18,7 +18,7 @@ describe("whiteboard", function() {
         _.on("StrokeAction.*.commit", function() {
           called = true;
         });
-        _.emit('StrokeAction.asdfasdf.commit', {stroke: stroke});
+        _.emit('StrokeAction.asdfasdf.commit', {target: target});
         expect(called).to.be(true);
       });
 
@@ -28,11 +28,11 @@ describe("whiteboard", function() {
           called = true;
         });
 
-        _.emit("this.what.commit", {stroke: stroke});
+        _.emit("this.what.commit", {target: target});
         expect(called).to.be(true);
 
         called = false;
-        _.emit("that.why.commit", {stroke: stroke});
+        _.emit("that.why.commit", {target: target});
         expect(called).to.be(true);
 
       });
@@ -45,11 +45,11 @@ describe("whiteboard", function() {
           called = true;
         };
         _.on("StrokeAction.focus.commit", myFun);
-        _.emit('StrokeAction.focus.commit', {stroke: stroke});
+        _.emit('StrokeAction.focus.commit', {target: target});
         expect(called).to.be(true);
         called = false;
         _.off("StrokeAction.focus.commit", myFun);
-        _.emit("StrokeAction.focus.commit", {stroke: stroke});
+        _.emit("StrokeAction.focus.commit", {target: target});
         expect(called).to.be(false);
       });
     });
