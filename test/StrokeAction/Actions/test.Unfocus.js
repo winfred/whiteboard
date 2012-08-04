cb5cec5fa91a57d9798e089028e2a086a4023a58
Winfred <winfred.nadeau@gmail.com>
2012-08-03 23:29:53 -0400

    add Unfocus.js test

---
+++ test/StrokeAction/Actions/test.Unfocus.js
@@ -0,0 +1,16 @@
+describe("StrokeAction.unfocus", function() {
+	var _ = whiteboard,
+			$ = whiteboard.StrokeAction;
+	
+	describe("commit", function() {
+		it("removes the focused style from the currently focused element", function() {
+			var stroke = _.test.helpers.paintStroke();
+			$.focus.invoke({currentTarget: stroke});
+			$.unfocus.target = stroke;
+			$.unfocus.commit();
+			expect(stroke.hasClass('whiteboard-focused')).to.be(false);
+			_.htmlTag.removeChild(stroke);
+		});
+	});
+
+});
