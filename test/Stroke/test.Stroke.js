521d2bd08a6ff2351f339c67dcb1a5e954ae73e3
Winfred <winfred.nadeau@gmail.com>
2012-08-10 14:36:22 -0400

    update tests to reflect stroke.removeFromDOM change

--- test/Stroke/test.Stroke.js
+++ test/Stroke/test.Stroke.js
@@ -1,19 +1,19 @@
 describe("Stroke", function() {
-	var _ = whiteboard;
-	describe(".includedBy", function() {
-		it("attaches Stroke module functions to the provided object", function() {
-			var stroke = _.test.helpers.paintStroke();
-			_.Stroke.includedBy(stroke);
-			expect(typeof stroke.serialize).to.be('function');
-			_.htmlTag.removeChild(stroke);
-		});
-	});
+  var _ = whiteboard;
+  describe(".includedBy", function() {
+    it("attaches Stroke module functions to the provided object", function() {
+      var stroke = _.test.helpers.paintStroke();
+      _.Stroke.includedBy(stroke);
+      expect(typeof stroke.serialize).to.be('function');
+      stroke.removeFromDOM();
+    });
+  });
 
   describe(".create", function() {
     it("focuses the recently created stroke", function() {
       var stroke = _.Stroke.create(_.Brush.get('arrow'));
       expect(stroke.hasClass('whiteboard-focused')).to.be(true);
-      _.htmlTag.removeChild(stroke);
+      stroke.removeFromDOM();
     });
 
     it("emits a Stroke.create.commit event", function() {
@@ -27,36 +27,36 @@ describe("Stroke", function() {
       stroke = _.Stroke.create(_.Brush.get('arrow'));
       expect(handlerCalled).to.be(true);
       expect(module).to.be("Stroke");
-      _.htmlTag.removeChild(stroke);
+      stroke.removeFromDOM();
     });
   });
-	describe("#serialize", function(){
+  describe("#serialize", function(){
 
-		var stroke, serialized;
+    var stroke, serialized;
 
-		beforeEach(function() {
-			stroke = _.test.helpers.paintStroke();
-			serialized = stroke.serialize();
-		});
+    beforeEach(function() {
+      stroke = _.test.helpers.paintStroke();
+      serialized = stroke.serialize();
+    });
 
-		afterEach(function() {
-			_.htmlTag.removeChild(stroke);
-		});
+    afterEach(function() {
+      stroke.removeFromDOM();
+    });
 
-		it("returns a POJO of attributes", function() {
-			expect(serialized).to.be.a('object');
-		});
+    it("returns a POJO of attributes", function() {
+      expect(serialized).to.be.a('object');
+    });
 
-		it("has the stroke's id", function() {
-			expect(serialized.id).to.be.a('string');	
-		});
+    it("has the stroke's id", function() {
+      expect(serialized.id).to.be.a('string');  
+    });
 
-		it("has the stroke's brush name", function() {
-			expect(serialized.brush).to.be.a('string');
-		});
+    it("has the stroke's brush name", function() {
+      expect(serialized.brush).to.be.a('string');
+    });
 
-		it("has the stroke's html content (for now)", function() {
-		  expect(serialized.html[0]).to.be("<");
-		});
-	});
+    it("has the stroke's html content (for now)", function() {
+      expect(serialized.html[0]).to.be("<");
+    });
+  });
 });
--- test/StrokeAction/Actions/test.Focus.js
+++ test/StrokeAction/Actions/test.Focus.js
@@ -1,14 +1,15 @@
 describe("StrokeAction.focus", function() {
-	var _ = whiteboard,
-	    $ = whiteboard.StrokeAction;
-	
-	describe("commit", function() {
-		it("applies a whiteboard-focused style to the target element", function() {
-			var stroke = _.test.helpers.paintStroke();
-			$.focus.invoke({currentTarget: stroke});
-			expect(stroke.hasClass('whiteboard-focused')).to.be(true);
-			_.htmlTag.removeChild(stroke);
-		});
-	});
+  var _ = whiteboard,
+      $ = whiteboard.StrokeAction;
+  
+  describe("commit", function() {
+    it("applies a whiteboard-focused style to the target element", function() {
+      var stroke = _.test.helpers.paintStroke();
+      $.focus.invoke({currentTarget: stroke});
+      expect(stroke.hasClass('whiteboard-focused')).to.be(true);
+      _.htmlTag.removeChild(stroke);
+      stroke.removeFromDOM();
+    });
+  });
 
 });
